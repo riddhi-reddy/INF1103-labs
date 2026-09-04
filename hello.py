4595ab491f98e891967091ca77c7acf8aa2965c2
Riddhi Reddy <2603622@sit.singaporetech.edu.sg>
2026-09-04 13:13:49 +0800

    Add Instagram Profile Details Activity 2

--- hello.py
+++ hello.py
@@ -1,4 +1,22 @@
+'''
+# Activity 1
 print("============================================")
 print("Welcome here")
 print("My first post!")
 print("============================================")
+
+'''
+
+#Activity 2
+print("============================================")
+print("Welcome here")
+print("My first post!")
+print("============================================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
