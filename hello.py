1be2159fd786b08e6b3cfb3d9644533964da334c
Riddhi Reddy <2603622@sit.singaporetech.edu.sg>
2026-09-04 13:17:36 +0800

    Add Follower Growth Tracker Activity 3

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 '''
-# Activity 1
+# Activity 1: First Post
 print("============================================")
 print("Welcome here")
 print("My first post!")
@@ -7,7 +7,8 @@ print("============================================")
 
 '''
 
-#Activity 2
+'''
+#Activity 2: Instagram Profile
 print("============================================")
 print("Welcome here")
 print("My first post!")
@@ -20,3 +21,26 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+'''
+
+# Activity 3: Follower Growth Tracker
+print("============================================")
+print("Welcome here")
+print("My first post!")
+print("============================================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
